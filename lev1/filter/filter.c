b480eed4f6e9478cf8e0a02b955643a7e28d3392
Hideumi Abe <habe@c4r1s5.42tokyo.jp>
2026-02-24 17:15:24 +0900

    filter.c && n_queens.c解

---
+++ lev1/filter/filter.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   filter.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: habe <habe@student.42tokyo.jp>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/24 17:08:04 by habe              #+#    #+#             */
+/*   Updated: 2026/02/24 17:10:51 by habe             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#ifndef BUF_SIZE
+# define BUF_SIZE 100
+#endif
+
+char	*read_stdin(void)
+{
+	char	*src;
+	size_t	total_size;
+	size_t	read_size;
+
+	src = NULL;
+	total_size = 0;
+	while (1)
+	{
+		src = realloc(src, total_size + BUF_SIZE + 1);
+		if (src == NULL)
+			return (NULL);
+		read_size = read(0, src + total_size, BUF_SIZE);
+		if (read_size == 0)
+			break ;
+		if (read_size < 0)
+			return (NULL);
+		total_size += read_size;
+	}
+	src[total_size] = '\0';
+	return (src);
+}
+
+void	filter_src(char *src, char *key, size_t key_len)
+{
+	char	*tmp;
+	char	*current_ptr;
+	size_t	count;
+
+	current_ptr = src;
+	while (1)
+	{
+		count = 0;
+		tmp = memmem(current_ptr, strlen(current_ptr), key, key_len);
+		if (tmp == NULL || *tmp == '\0')
+			break ;
+		while (count < key_len)
+		{
+			tmp[count] = '*';
+			count++;
+		}
+		current_ptr = tmp + key_len;
+	}
+}
+
+int	main(int ac, char **av)
+{
+	char	*src;
+
+	if (ac != 2 || av[1][0] == '\0')
+		return (printf("ERROR\n"), 1);
+	src = read_stdin();
+	if (src == NULL)
+		return (perror("ERROR\n"), 1);
+	filter_src(src, av[1], strlen(av[1]));
+	printf("%s", src);
+	free(src);
+	return (0);
+}
---
+++ lev2/n_queens/n_queens.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   n_queens.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: habe <habe@student.42tokyo.jp>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/24 17:08:12 by habe              #+#    #+#             */
+/*   Updated: 2026/02/24 17:13:53 by habe             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int	is_valid(int *pos, int row, int col)
+{
+	int	i;
+
+	i = 0;
+	while (i < row)
+	{
+		if (pos[i] == col || pos[i] - col == i - row || pos[i] - col == row - i)
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+void	solve(int *pos, int row, int n)
+{
+	int	col;
+	int	i;
+
+	if (row == n)
+	{
+		i = 0;
+		while (i < n)
+		{
+			if (i > 0)
+				printf(" ");
+			printf("%d", pos[i]);
+			i++;
+		}
+		printf("\n");
+		return ;
+	}
+	col = 0;
+	while (col < n)
+	{
+		if (is_valid(pos, row, col) != 0)
+		{
+			pos[row] = col;
+			solve(pos, row + 1, n);
+		}
+		col++;
+	}
+}
+
+int	main(int ac, char *av[])
+{
+	int	n;
+	int	*pos;
+
+	if (ac != 2 || av[1][0] == '-')
+		return (1);
+	n = atoi(av[1]);
+	pos = malloc(sizeof(int) * n);
+	if (pos == NULL)
+		return (1);
+	solve(pos, 0, n);
+	free(pos);
+	return (0);
+}
